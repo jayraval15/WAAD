91bdfc2b338bc56e656ce81c5c07627ce9c02780
devloperli <132671668+devloperli@users.noreply.github.com>
2023-06-06 17:02:58 +0530

    image

--- api.php
+++ api.php
@@ -4,6 +4,23 @@ require_once("contion.php");
 $jsonedata = file_get_contents("php://input");
 $data = json_decode($jsonedata,true);
 
+$tampname = $_FILES['data']['tmp_name'];
+$target_folder = "image";
+$target_filename = $_FILES['data']['name'];
+$target_filesize =$_FILES['data']['size'];
+$target_fileerror = $_FILES['data']['error'];
+$target_filetype = $_FILES['data']['type'];
+
+$target_file = $target_folder."/".basename($target_filename);
+
+if($target_filesize > 2097152){
+    $error[]= "you file musst be 2MB";
+}
+
+if($target_fileerror==0){
+    $file_status = move_uploaded_file($tampname,$target_file);
+}
+
 $name = ($data['name']);
 $email = ($data['email']);
 $pass = ($data['pass']);
@@ -15,8 +32,8 @@ $dob = ($data['dob']);
 $latitude = ($data['latitude']);
 $longitude = ($data['longitude']);
 
-$query = "INSERT INTO `$tablename`(`name`,`email`,`pass`,`cpass`,`mob`,`gender`,`city`,`dob`,`latitude`,`longitude`) 
-value ('$name','$email','$pass','$cpass','$mob','$gender','$city','$dob','$latitude','$longitude')";
+$query = "INSERT INTO `$tablename`(`image`,`name`,`email`,`pass`,`cpass`,`mob`,`gender`,`city`,`dob`,`latitude`,`longitude`) 
+value ('$target_filename',$name','$email','$pass','$cpass','$mob','$gender','$city','$dob','$latitude','$longitude')";
 
 $result = mysqli_query($conn,$query);
 
