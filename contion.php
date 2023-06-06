5f2b3619d5629da5b8df0fbb3e9b97d6e1dbc3b1
devloperli <132671668+devloperli@users.noreply.github.com>
2023-06-06 17:50:47 +0530

    uppp

--- contion.php
+++ contion.php
@@ -1,9 +1,9 @@
 <?php
 $hostname = "localhost";
-$user = "root";
-$dbpass = "";
-$dbname = "waad";
-$tablename = "waad-project";
+$user = "id20676639_insorans_23"; // your servar user name.//
+$dbpass = "MKL?php1";  // your servar password.//
+$dbname = "id20676639_life_time_12";  // your servar databesh name.//
+$tablename = "waad-project";  // your servar tablename name.//
 
 $conn = mysqli_connect($hostname,$user,$dbpass,$dbname) or die("CONTION ERROR");
 
