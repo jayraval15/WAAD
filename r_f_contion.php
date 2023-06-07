bc5b8976be5957f2f31fcbbce3e7e0e20542d193
jayraval15 <132762504+jayraval15@users.noreply.github.com>
2023-06-07 20:02:36 +0530

    assas

---
+++ r_f_contion.php
@@ -0,0 +1,44 @@
+<?php
+require_once("contion.php");
+
+$tampname = $_FILES['pic']['tmp_name'];
+$target_folder = "photo";
+$target_filename = $_FILES['pic']['name'];
+$target_filesize =$_FILES['pic']['size'];
+$target_fileerror = $_FILES['pic']['error'];
+$target_filetype = $_FILES['pic']['type'];
+
+$target_file = $target_folder."/".basename($target_filename);
+
+if($target_filesize > 2097152){
+    $error[]= "you file musst be 2MB";
+}
+
+if($target_fileerror==0){
+    $file_status = move_uploaded_file($tampname,$target_file);
+}
+
+$name = ($_POST['name']);
+$email = ($_POST['email']);
+$pass = ($_POST['pass']);
+$cpass = ($_POST['cpass']);
+$mob = ($_POST['mob']);
+$gender = ($_POST['gender']);
+$city = ($_POST['city']);
+$dob = ($_POST['dob']);
+$latitude = ($_POST['latitude']);
+$longitude = ($_POST['longitude']);
+$photo = $_FILES['image']['name'];
+
+$query = "INSERT INTO `$tablename` (`image`,`name`,`email`,`pass`,`cpass`,`mob`,`gender`,`city`,`dob`,`latitude`,`longitude`) 
+value ('{$photo}','{$name}','{$email}','{$pass}','{$cpass}','{$mob}','{$gender}','{$city}','{$dob}','{$latitude}','{$longitude}')";
+
+$result = mysqli_query($conn,$query) or die("RESULT ERROR");
+
+if($result==1){
+    echo "";
+}else{
+    echo "result if erre";
+}
+
+?>
--- registration_form.php
+++ registration_form.php
@@ -1,8 +1,13 @@
+<?php
+
+
+include "contion.php";
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Login Form</title>
-<link rel="stylesheet" href="style.css">
+<title>sig in Form</title>
+<link rel="stylesheet" href="r_style.css">
 </head>
 <body>
 
@@ -10,24 +15,25 @@
 <img src="avatar1.png">
 <p class="text">User Login</p>
 
-<form class="login_form">
+<form class="login_form" action="r_f_contion.php" method="post">
+    <input type="file" name="pic" id="">
 <input type="text" name="name" placeholder="Enter yor name" required><br>
-<input type="email" name="" placeholder="Enter yor email" required><br>
-<input type="password" name="" placeholder="Enter yor password" required><br>
-<input type="password" name="" placeholder="Enter yor match password" required><br>
-<input type="number" name="" placeholder="Enter yor number" required><br>
+<input type="email" name="email" placeholder="Enter yor email" required><br>
+<input type="password" name="pass" placeholder="Enter yor password" required><br>
+<input type="password" name="cpass" placeholder="Enter yor match password" required><br>
+<input type="number" name="mob" placeholder="Enter yor number" required><br>
 <div class="gender">
-<input type="radio" style="height: 20px " name="gander" id="" value="1">Male
-<input type="radio" style="height: 20px;" name="gander" id="" value="2">Female
+<input type="radio" style="height: 20px" name="gander" id="" value="1" Male>
+<input type="radio" style="height: 20px" name="gander" id="" value="2" Female>
 </div>
-<input type="text" name="city" id="" required>
+<input type="text" name="city" placeholder="Enter yor city" id="" required>
 <input type="date" name="dob" id="" required>
-<input type="text" name="latitude" id="" required>
-<input type="text" name="longitude" id="" required>
+<input type="text" name="latitude" placeholder="Enter yor latitude"  id="" required>
+<input type="text" name="longitude" placeholder="Enter yor longitude" id="" required>
 
-<button type="submit" name="login">Login</button>
+<button type="submit" name="login">sig in</button>
 <br><br><br><br>
-<div  style="-webkit-text-fill-color:aliceblue"> pless enter the new user req <a href="registration_form.php">sig in</a></div>
+<div  style="-webkit-text-fill-color:aliceblue"> pless enter the new user req <a href="login_form.php">log in</a></div>
 </form>
 
 </div>
