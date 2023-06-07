f086eab58c2e74823cfc47f4afbf3a1b416279fc
devloperli <132671668+devloperli@users.noreply.github.com>
2023-06-07 18:10:43 +0530

    update

--- contion.php
+++ contion.php
@@ -1,8 +1,14 @@
 <?php
+// $hostname = "localhost";
+// $user = "id20676639_insorans_23"; // your servar user name.//
+// $dbpass = "MKL?php1";  // your servar password.//
+// $dbname = "id20676639_life_time_12";  // your servar databesh name.//
+// $tablename = "waad-project";  // your servar tablename name.//
+
 $hostname = "localhost";
-$user = "id20676639_insorans_23"; // your servar user name.//
-$dbpass = "MKL?php1";  // your servar password.//
-$dbname = "id20676639_life_time_12";  // your servar databesh name.//
+$user = "root"; // your servar user name.//
+$dbpass = "";  // your servar password.//
+$dbname = "waad";  // your servar databesh name.//
 $tablename = "waad-project";  // your servar tablename name.//
 
 $conn = mysqli_connect($hostname,$user,$dbpass,$dbname) or die("CONTION ERROR");
---
+++ login_form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="form_body">
+<img src="avatar1.png">
+<p class="text">User Login</p>
+
+<form class="login_form">
+<input type="text" name="" placeholder="Email or Username"><br>
+<input type="password" name="" placeholder="Password"><br>
+<button type="submit" name="login">Login</button>
+<br><br><br><br>
+<div  style="-webkit-text-fill-color:aliceblue"> pless enter the new user req <a href="registration_form.php">sig in</a></div>
+</form>
+
+</div>
+
+</body>
+</html>
