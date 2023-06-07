9f86d59b5df8f01dedf62f74b67caacdb9351936
devloperli <132671668+devloperli@users.noreply.github.com>
2023-06-07 18:10:29 +0530

    commant

---
+++ registration_form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="form_body">
+<img src="avatar1.png">
+<p class="text">User Login</p>
+
+<form class="login_form">
+<input type="text" name="name" placeholder="Enter yor name" required><br>
+<input type="email" name="" placeholder="Enter yor email" required><br>
+<input type="password" name="" placeholder="Enter yor password" required><br>
+<input type="password" name="" placeholder="Enter yor match password" required><br>
+<input type="number" name="" placeholder="Enter yor number" required><br>
+<div class="gender">
+<input type="radio" style="height: 20px " name="gander" id="" value="1">Male
+<input type="radio" style="height: 20px;" name="gander" id="" value="2">
+
+Female
+</div>
+<button type="submit" name="login">Login</button>
+<br><br><br><br>
+<div  style="-webkit-text-fill-color:aliceblue"> pless enter the new user req <a href="registration_form.php">sig in</a></div>
+</form>
+
+</div>
+
+</body>
+</html> 
